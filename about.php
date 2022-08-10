238acd87fcfde084de375ab9f387ec50cc7c5a7d
Harsh Babaria <103020994+harsh7444@users.noreply.github.com>
2022-08-10 14:30:39 +0530

    Update about.php

--- about.php
+++ about.php
@@ -434,6 +434,9 @@
           </div>
         </div>
       </section>
+      <a href="https://api.whatsapp.com/send?phone=9998633336" class="float" target="_blank">
+        <i class="fa fa-whatsapp my-float"></i>
+      </a>
       <footer id="footer">
         <div class="footer-widgets">
           <div class="container">
