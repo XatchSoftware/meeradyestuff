24e5e611b076f7f28ad1a95c7980ccb97d8edb5b
Harsh Babaria <103020994+harsh7444@users.noreply.github.com>
2022-08-10 14:38:18 +0530

    Update quality.php

--- quality.php
+++ quality.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="css/bundle.css">
     <link rel="stylesheet" href="css/hody-icons.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700|Source+Sans+Pro:300,400|Kaushan+Script|Lora|Playfair+Display:700" rel="stylesheet" type="text/css">
     <style>
       
